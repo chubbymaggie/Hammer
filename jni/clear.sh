9e9b3b9451b314e6f500c82d234e0d10dc6b7c1c
tb716139 <tobias.bieschke@rwth-aachen.de>
2015-11-19 02:11:10 +0100

    Maven Support Added

---
+++ jni/clear.sh
@@ -0,0 +1,1 @@
+rm -f com/upstandinghackers/hammer/*.class && rm -f com_upstandinghackers_*.h && rm -f com_upstandinghackers_*.o && rm -f libjhammer.so
---
+++ jni/compile.sh
@@ -0,0 +1,8 @@
+mkdir ../target
+mkdir ../target/classes
+mkdir ../target/classes
+mkdir ../target/classes/com
+mkdir ../target/classes/com/upstandinghackers
+mkdir ../target/classes/com/upstandinghackers/hammer
+cp libjhammer.so ../target/classes/libjhammer.so
+cp -r com/upstandinghackers/hammer/*.class ../target/classes/com/upstandinghackers/hammer/
