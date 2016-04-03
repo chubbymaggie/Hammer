272d64f565411ae13c8d597e2e19d80c51cba86d
tb716139 <tobias.bieschke@rwth-aachen.de>
2016-04-03 17:52:03 +0200

    added build/*.os files to resources in hammer.jar

--- jni/compile.sh
+++ jni/compile.sh
@@ -4,6 +4,11 @@ mkdir ../target/classes/resources
 mkdir ../target/classes/resources/include
 mkdir ../target/classes/resources/lib
 mkdir ../target/classes/resources/src
+mkdir ../target/classes/resources/build
+mkdir ../target/classes/resources/build/opt
+mkdir ../target/classes/resources/build/opt/src
+mkdir ../target/classes/resources/build/opt/src/backends
+mkdir ../target/classes/resources/build/opt/src/parsers
 mkdir ../target/classes/com
 mkdir ../target/classes/com/upstandinghackers
 mkdir ../target/classes/com/upstandinghackers/hammer
@@ -15,3 +20,6 @@ cp -r com/upstandinghackers/hammer/*.class ../target/classes/com/upstandinghacke
 cp ../src/*.h ../target/classes/resources/include/
 cp ../lib/*.c ../target/classes/resources/src/
 cp ../lib/hush ../target/classes/resources/lib/
+cp ../build/opt/src/*.os ../target/classes/resources/build/opt/src
+cp ../build/opt/src/backends/*.os ../target/classes/resources/build/opt/src/backends
+cp ../build/opt/src/parsers/*.os ../target/classes/resources/build/opt/src/parsers
