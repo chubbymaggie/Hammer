7c8689b6e7d832396e23fa21d0c83942746b65e3
tb716139 <tobias.bieschke@rwth-aachen.de>
2016-04-03 15:44:04 +0200

    copying more files to hammer.jar

--- jni/compile.sh
+++ jni/compile.sh
@@ -1,8 +1,17 @@
 mkdir ../target
 mkdir ../target/classes
-mkdir ../target/classes
+mkdir ../target/classes/resources
+mkdir ../target/classes/resources/include
+mkdir ../target/classes/resources/lib
+mkdir ../target/classes/resources/src
 mkdir ../target/classes/com
 mkdir ../target/classes/com/upstandinghackers
 mkdir ../target/classes/com/upstandinghackers/hammer
-cp libjhammer.so ../target/classes/libjhammer.so
+cp libjhammer.so ../target/classes/resources/libjhammer.so
+cp jhammer.h ../target/classes/resources/jhammer.h
+cp ../common.mk ../target/classes/resources/common.mk
+cp ../config.mk ../target/classes/resources/config.mk
 cp -r com/upstandinghackers/hammer/*.class ../target/classes/com/upstandinghackers/hammer/
+cp ../src/*.h ../target/classes/resources/include/
+cp ../lib/*.c ../target/classes/resources/src/
+cp ../lib/hush ../target/classes/resources/lib/
